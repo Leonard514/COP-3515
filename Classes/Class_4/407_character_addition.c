83e8f9e3a0fcb11a1271373adc82241b65b14257
Leonard514 <leonardalwright@gmail.com>
2024-09-09 22:50:30 +0000

    Character addition test

---
+++ Classes/Class_4/407_character_addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    int ac = 'a' + 'c';
+    int as = 'a' + 's';
+    int bc = 'b' + 'c';
+    int bs = 'b' + 's';
+
+    printf("%d %d %d %d", ac, as, bc, bs);
+}
