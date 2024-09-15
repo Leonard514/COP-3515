d44a6c3c603fdfa09bcf7cfa2891a5ab3a95a85b
Leonard514 <leonardalwright@gmail.com>
2024-09-15 00:59:15 +0000

    Changes w/ Anthony study session

--- Classes/Class_5/503_type_casting.c
+++ Classes/Class_5/503_type_casting.c
@@ -2,15 +2,16 @@
 
 int main()
 {
-    char sample = '1';
-    int sample = 1;
+    char sample = "";
+    char sample1 = '1';
+    int sample2 = 1;
 
     // Notice type casting a character will utilize its ASCII value always
     // This means we can't convert "numeric" strings to integers that easily,
     // but that's not a problem since in C we can take numeric input (whereas)
     // in Python we could only take string input
     
-    printf("1 + 2 = %d\n", (int)sample + 2);
+    printf("1 + 2 = %d\n", (int)sample1 + 2);
 
     return(0);
 }
